91ffb4ea540df477765b262cc700773d85a83e39
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-16 14:24:52 +0100

    Prepare game view

--- js/blackjack/Game.js
+++ js/blackjack/Game.js
@@ -1,5 +1,43 @@
 export class Game {
-    constructor() {
-        console.log("New game starts!")
+    GAME_STATE = {
+        MENU: 0,
+        RUNNING: 1,
+        TOPSCORE: 2
+    }
+
+    constructor(windowDiv, menuDiv, gameDiv) {
+        this.windowDiv = windowDiv;
+        this.menuDiv = menuDiv;
+        this.gameDiv = gameDiv;
+        this.gamestate = this.GAME_STATE.RUNNING;
+        this.balance = 1000;
+
+        console.log("New game starts!");
+        this.hideElement(menuDiv);
+        this.setFullWindow();
+    }
+
+    startGame() {
+        this.hideElement(menuDiv);
+        this.setFullWindow();
+    }
+
+    setFullWindow() {
+        this.windowDiv.classList.remove('col-8');
+        this.windowDiv.classList.remove('offset-2');
+        this.windowDiv.classList.add('col-12');
+        this.windowDiv.style.height = "80vh";
+    }
+
+    hideElement(element) {
+        console.log("hiding");
+        element.classList.remove('d-flex');
+        element.classList.add('d-none');
+    } 
+
+    showElement(element) {
+        console.log("showing");
+        element.classList.remove('d-none');
+        element.classList.add('d-flex');
     }
 }
--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ import { Game } from "./blackjack/Game.js";
 const btnNewGame = document.getElementById('btnNewGame');
 const btnLoadGame = document.getElementById('btnLoadGame');
 const btnTopScore = document.getElementById('btnTopScore');
+const windowDiv = document.getElementById('windowDiv');
+const gameDiv = document.getElementById('gameDiv');
+const menuDiv = document.getElementById('menuDiv');
 
 btnNewGame.addEventListener('click', () => {
-    const game = new Game();
+    const game = new Game(windowDiv, menuDiv, gameDiv);
 })
